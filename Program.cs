52377c0d0b7439f949a0fb26fcad03b1fa174fe1
William Cram <FDNeo_83@hotmail.com>
2021-07-31 21:00:28 -0700

    Modify Console

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Code_Base
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Me");
         }
     }
 }
